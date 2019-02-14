bf5f0c0bfe7c41917c231f9a098e3ab6d487299d
Joey Leger <legerinvestments@gmail.com>
2019-02-14 09:03:06 -0500

    [ADD] Cleaned things up a little bit. Moved everything to an init function

--- scraper.js
+++ scraper.js
@@ -1,24 +1,28 @@
 const puppeteer = require('puppeteer');
 
-const getInfo1 = async (url) => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(url, {waitUntil: 'networkidle2'});
-        const innerText = await page.evaluate(() => document.querySelector('.sidebar-download').innerText);
-        const answer = innerText.slice(10, innerText.length);
-        console.log('AwEEsome Icons', answer);
-        await browser.close();
+let browser;
+let page;
+
+const awesomeIcons = async (url) => {
+    await page.goto(url, {waitUntil: 'networkidle2'});
+    const innerText = await page.evaluate(() => document.querySelector('.sidebar-download').innerText);
+    const answer = innerText.slice(10, innerText.length);
+    console.log('AwEEsome Icons', answer);
 };
 
-const getInfo2 = async (url) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+const calendar = async (url) => {
     await page.goto(url, {waitUntil: 'networkidle2'});
     const innerText = await page.evaluate(() => document.querySelector('.content > h2').innerText);
     const answer = innerText.slice(1, -13).trim();
     console.log('Calendar', answer);
-    await browser.close();
 };
 
-getInfo1('https://devot-ee.com/add-ons/aweesome-icons');
-getInfo2('http://docs.solspace.com/expressionengine/calendar/v3/setup/changelog.html');
+const init = async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    await awesomeIcons('https://devot-ee.com/add-ons/aweesome-icons');
+    await calendar('http://docs.solspace.com/expressionengine/calendar/v3/setup/changelog.html');
+    await browser.close();
+}
+
+init();
