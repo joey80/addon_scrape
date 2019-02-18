9e725c3e2126382bcc6fdd7dc74c4eda11474ef8
Joey Leger <legerinvestments@gmail.com>
2019-02-18 15:57:23 -0500

    latest build

--- scraper.js
+++ scraper.js
@@ -3,6 +3,7 @@ const puppeteer = require('puppeteer');
 let browser;
 let page;
 let results = [];
+let version;
 const addOns = [
     {
         "name": "awEEsome Icons",
@@ -58,19 +59,125 @@ const addOns = [
         "name": "Detour Pro",
         "url": "https://eeharbor.com/detour-pro/changelog",
         "element": "div.changelog:nth-child(1) > h2:nth-child(2)"
+    },
+    {
+        "name": "Field Limits",
+        "url": "https://buzzingpixel.com/software/field-limits/changelog",
+        "element": "div.block-changelog:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(2)"
+    },
+    {
+        "name": "Field Pack",
+        "url": "https://eeharbor.com/fieldpack/changelog",
+        "element": "div.changelog:nth-child(1) > h2:nth-child(2)"
+    },
+    {
+        "name": "Forms",
+        "url": "https://eeharbor.com/forms",
+        "element": "div.release-version:nth-child(2) > span:nth-child(2)"
+    },
+    {
+        "name": "FreeForm",
+        "url": "http://docs.solspace.com/expressionengine/freeform/v1/setup/changelog.html",
+        "element": "html body div#app div.theme-container main.page div.content.default h2:first-of-type"
+    },
+    {
+        "name": "Low Reorder",
+        "url": "https://gotolow.com/addons/low-reorder/changelog",
+        "element": ".addon-meta > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)"
+    },
+    {
+        "name": "Low Search",
+        "url": "https://gotolow.com/addons/low-search/changelog",
+        "element": ".addon-meta > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)"
+    },
+    {
+        "name": "Low Seg2Cat",
+        "url": "https://gotolow.com/addons/low-seg2cat/changelog",
+        "element": ".addon-meta > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)"
+    },
+    {
+        "name": "Matrix",
+        "url": "https://eeharbor.com/matrix/changelog",
+        "element": "div.changelog:nth-child(1) > h2:nth-child(2)"
+    },
+    {
+        "name": "Mo' Variables",
+        "url": "https://devot-ee.com/add-ons/mo-variables",
+        "element": ".btn-standard"
+    },
+    {
+        "name": "Playa",
+        "url": "https://eeharbor.com/playa/changelog",
+        "element": "div.changelog:nth-child(1) > h2:nth-child(2)"
+    },
+    {
+        "name": "Publisher Lite",
+        "url": "https://boldminded.com/add-ons/publisher",
+        "element": ".nav_content_right"
+    },
+    {
+        "name": "Safe Harbor",
+        "url": "https://eeharbor.com/safeharbor/changelog",
+        "element": "div.changelog:nth-child(1) > h2:nth-child(2)"
+    },
+    {
+        "name": "SEO Lite",
+        "url": "https://devot-ee.com/add-ons/seolite",
+        "element": ".addon-download > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(3)"
+    },
+    {
+        "name": "Sitemap Module",
+        "url": "https://devot-ee.com/add-ons/sitemap-module",
+        "element": "#addon-commercial"
+    },
+    {
+        "name": "Snaptcha",
+        "url": "https://devot-ee.com/add-ons/snaptcha",
+        "element": "#addon-commercial"
+    },
+    {
+        "name": "Structure",
+        "url": "https://eeharbor.com/structure/changelog",
+        "element": "div.changelog:nth-child(1) > h2:nth-child(2)"
+    },
+    {
+        "name": "User",
+        "url": "https://eeharbor.com/user/changelog",
+        "element": "div.changelog:nth-child(1) > h2:nth-child(2)"
+    },
+    {
+        "name": "Visitor",
+        "url": "https://eeharbor.com/visitor/changelog",
+        "element": "div.changelog:nth-child(1) > h2:nth-child(2)"
+    },
+    {
+        "name": "Wygwma",
+        "url": "https://eeharbor.com/wygwam/changelog",
+        "element": "div.changelog:nth-child(1) > h2:nth-child(2)"
     }
 ];
 
 const getUpdate = async (name, url, element) => {
+    console.log('Retrieving', name + ' ...');
     await page.goto(url, {waitUntil: 'networkidle2'});
     const innerText = await page.$eval(element, el => el.innerText);
-    // Strip out the word '3rd' and any character that is not a number or decimal
-    const first = innerText.replace("3rd", "");
-    const second = first.replace(/[^0-9.]/g, "");
+
+    // Change this to seperate into arrays and only keep the one with decimals
+    const cleanIt = innerText.replace(/[^0-9.]/g, " ");
+    const splits = cleanIt.split(" ");
+
+    for (let split of splits) {
+        if (split.includes('.')) {
+            version = split;
+            break;
+        }
+    }
+
     let result = {
         "name": name,
-        "version": second
+        "version": version
     };
+
     results.push(result);
 };
 
@@ -78,8 +185,8 @@ const init = async () => {
     browser = await puppeteer.launch();
     page = await browser.newPage();
 
-    for (let i = 0; i < addOns.length; i++) {
-        await getUpdate(addOns[i].name, addOns[i].url, addOns[i].element);
+    for (let addOn of addOns) {
+       await getUpdate(addOn.name, addOn.url, addOn.element);
     }
 
     await browser.close();
